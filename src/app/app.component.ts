4cb40bbd4d3152d3d58b3e55c25b6ab9464393c3
jacquline <jacqulinewangu@gmail.com>
2019-04-29 12:42:18 +0300

    added file quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+
 })
 export class AppComponent {
-  title = QUOTES';
+   quotes = 'Instead';
+
+   constructor(){
+
+    this.quotes = [" Instead of always indulging in your desires, you need limits on those desires to be truly happy.","Appreciating the simpler things is a quicker route to happiness."];
+  }
 }
